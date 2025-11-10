b60f90d1951fed00d51f29d2b60e4ee48fbca211
lostmonster84 <james@lostmonster.co.uk>
2025-11-10 18:27:10 +0100

    fix: Add outputFileTracingRoot to resolve lockfile warning

--- website/next.config.js
+++ website/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: require('path').join(__dirname),
   images: {
     formats: ['image/avif', 'image/webp'],
     remotePatterns: [],
